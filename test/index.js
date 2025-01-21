62265c5d34836e6b47a67bb7628cdd7e29b1bf00
reguletorjustin <justinsebring3478@gmail.com>
2025-01-22 05:15:46 +0800

    feat: Add invoke & prompts to generate plan workflow

--- test/index.js
+++ test/index.js
@@ -1,10 +1,22 @@
 import { Lyvo }  from '../src/lyvo.js';
+import { ChatAnthropic } from "@langchain/anthropic";
 
-const lyvo = new Lyvo();
-
-const workflow = lyvo.generateWorkflow('');
-const result = lyvo.execute(workflow, {
-    callback (node) {
-        console.log(node);
-    }
+const model = new ChatAnthropic({
+  model: "claude-3-5-sonnet-20241022",
+  temperature: 0
 });
+
+const lyvo = new Lyvo(model);
+
+const workflow = await lyvo.invoke(`打开领英，搜索 Chromium 开发者，将前十页的信息整理成一个表格`);
+
+if(workflow !== null){
+    const result = lyvo.execute(workflow, {
+        callback (node) {
+            console.log(node);
+        }
+    });
+}
+
+
+// console.log(result)
