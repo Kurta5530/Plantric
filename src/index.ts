982182565793a84d1028235764660aecef8e0837
reguletorjustin <justinsebring3478@gmail.com>
2025-02-14 11:16:05 +0800

    feat: browser_use & openai-provider

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Lyvo from './core/lyvo';
 import { ToolRegistry } from './core/tool-registry';
 import { ClaudeProvider } from './services/llm/claude-provider';
+import { OpenaiProvider } from './services/llm/openai-provider';
 import { WorkflowParser } from './services/parser/workflow-parser';
 import { WorkflowGenerator } from "./services/workflow/generator"
 
@@ -8,6 +9,7 @@ export {
   Lyvo,
   WorkflowGenerator,
   ClaudeProvider,
+  OpenaiProvider,
   ToolRegistry,
   WorkflowParser
 }
